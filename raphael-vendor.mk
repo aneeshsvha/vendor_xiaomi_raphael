5b1976365ee6c8446369370a55fd1f9359379452
firebird11 <hbgassel@gmail.com>
2023-01-01 20:22:47 +0100

    raphael: Add camerax-vendor-extensions.jar
    Signed-off-by: SageOfD6Path <mail2anirban95@gmail.com>

--- raphael-vendor.mk
+++ raphael-vendor.mk
@@ -1263,6 +1263,7 @@ PRODUCT_PACKAGES += \
     dpmserviceapp \
     ims \
     qcrilmsgtunnel \
+    camerax-vendor-extensions \
     tcmclient \
     com.android.hotwordenrollment.common.util \
     com.qti.dpmframework \
